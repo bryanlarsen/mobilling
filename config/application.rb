2d38274b3f254526bffdac550e70a3e645365b7e
Kuba Kuźma <kuba@jah.pl>
2014-06-17 20:23:29 +0200

    added open sans

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,6 @@ module MoBilling
     config.middleware.use "AuthOverride"
     config.angular_templates.module_name = "moBilling.templates"
     config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
-    config.assets.precompile = %w[*.png *.jpg *.eot *.svg *.ttf *.woff]
+    config.assets.precompile = %w[*.png *.jpg *.eot *.svg *.ttf *.woff application.css application.js]
   end
 end
