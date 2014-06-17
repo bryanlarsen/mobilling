dbdd972e026bc8adb2a3dd0763d0961ce93ccfca
Kuba Kuźma <kuba@jah.pl>
2014-06-17 09:11:42 +0200

    changed to MoBilling

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_mobilling_session"
+Rails.application.config.session_store :cookie_store, key: "_mo_billing_session"
