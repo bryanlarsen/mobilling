9f2a974280eb97df507e93a11f894b0b689c1b58
Kuba Kuźma <kuba@jah.pl>
2014-06-17 20:12:00 +0200

    bumped assets version

--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,6 @@ module MoBilling
   class Application < Rails::Application
     config.middleware.use "AuthOverride"
     config.angular_templates.module_name = "moBilling.templates"
+    config.assets.version = "1.1"
   end
 end
