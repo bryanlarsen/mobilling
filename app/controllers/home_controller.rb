a15acd9da5d622a6183baf08ba1beb9e1f2577e7
Kuba Kuźma <kuba@jah.pl>
2014-06-18 10:06:16 +0200

    removed useless home controller docs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,2 @@
 class HomeController < ApplicationController
-  # DOC GENERATED AUTOMATICALLY: REMOVE THIS LINE TO PREVENT REGENARATING NEXT TIME
-  api :GET, "", "Show a home"
-  def show
-  end
 end
