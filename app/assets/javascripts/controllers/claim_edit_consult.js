bed5a0a34b6082c31001593124e14b862822b79c
Kuba Kuźma <kuba@jah.pl>
2014-06-24 18:38:21 +0200

    added consult type

--- app/assets/javascripts/controllers/claim_edit_consult.js
+++ app/assets/javascripts/controllers/claim_edit_consult.js
@@ -1,5 +1,25 @@
 angular.module("moBilling.controllers.claimEditConsult", [])
 
     .controller("ClaimEditConsultController", function ($scope) {
+        $scope.syncSwitches = function () {
+            $scope.claim.consult_type_general_er           = ($scope.claim.consult_type === "general_er");
+            $scope.claim.consult_type_general_non_er       = ($scope.claim.consult_type === "general_non_er");
+            $scope.claim.consult_type_comprehensive_er     = ($scope.claim.consult_type === "comprehensive_er");
+            $scope.claim.consult_type_comprehensive_non_er = ($scope.claim.consult_type === "comprehensive_non_er");
+            $scope.claim.consult_type_limited_er           = ($scope.claim.consult_type === "limited_er");
+            $scope.claim.consult_type_limited_non_er       = ($scope.claim.consult_type === "limited_non_er");
+        };
 
+        $scope.setConsultType = function (type) {
+            if ($scope.claim.consult_type === type) {
+                $scope.claim.consult_type = undefined;
+            } else {
+                $scope.claim.consult_type = type;
+            }
+
+            $scope.syncSwitches();
+            $scope.$apply();
+        };
+
+        $scope.syncSwitches();
     });
