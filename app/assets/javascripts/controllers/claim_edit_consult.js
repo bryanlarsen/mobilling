34bebf3755eefd2d81e5d41271fdfab8a5e754f0
Kuba Kuźma <kuba@jah.pl>
2014-06-25 17:06:56 +0200

    added premium visit watcher

--- app/assets/javascripts/controllers/claim_edit_consult.js
+++ app/assets/javascripts/controllers/claim_edit_consult.js
@@ -3,6 +3,13 @@ angular.module("moBilling.controllers.claimEditConsult", [])
     .controller("ClaimEditConsultController", function ($scope) {
         $scope.isPremiumVisitVisible = !!($scope.claim.consult_premium_visit || $scope.claim.consult_premium_travel);
 
+        $scope.$watch("isPremiumVisitVisible", function (isPremiumVisitVisible) {
+            if (!isPremiumVisitVisible) {
+                $scope.claim.consult_premium_visit = undefined;
+                $scope.claim.consult_premium_travel = undefined;
+            }
+        });
+
         $scope.$watch($scope.isConsultVisible, function (isConsultVisible) {
             if (!isConsultVisible) {
                 $scope.claim.consult_type = undefined;
