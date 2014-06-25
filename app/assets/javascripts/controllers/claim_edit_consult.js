63bf3d99ab02d638b358108ec2a13df1656d7779
Kuba Kuźma <kuba@jah.pl>
2014-06-25 11:23:21 +0200

    clearing time_in, time_out when invisible

--- app/assets/javascripts/controllers/claim_edit_consult.js
+++ app/assets/javascripts/controllers/claim_edit_consult.js
@@ -1,29 +1,14 @@
 angular.module("moBilling.controllers.claimEditConsult", [])
 
     .controller("ClaimEditConsultController", function ($scope) {
-        function syncSwitches() {
-            $scope.claim.consult_type_general_er           = ($scope.claim.consult_type === "general_er");
-            $scope.claim.consult_type_general_non_er       = ($scope.claim.consult_type === "general_non_er");
-            $scope.claim.consult_type_comprehensive_er     = ($scope.claim.consult_type === "comprehensive_er");
-            $scope.claim.consult_type_comprehensive_non_er = ($scope.claim.consult_type === "comprehensive_non_er");
-            $scope.claim.consult_type_limited_er           = ($scope.claim.consult_type === "limited_er");
-            $scope.claim.consult_type_limited_non_er       = ($scope.claim.consult_type === "limited_non_er");
-        };
-
-        $scope.toggleConsultType = function (type) {
-            if ($scope.claim.consult_type === type) {
-                $scope.claim.consult_type = undefined;
-            } else {
-                $scope.claim.consult_type = type;
+        $scope.$watch("claim.consult_type", function (consult_type) {
+            if (!$scope.isTimeVisible()) {
+                $scope.claim.consult_time_in = undefined;
+                $scope.claim.consult_time_out = undefined;
             }
-
-            syncSwitches();
-            $scope.$apply();
-        };
+        });
 
         $scope.isTimeVisible = function () {
             return $scope.claim.consult_type && $scope.claim.consult_type.indexOf("comprehensive") === 0;
         };
-
-        syncSwitches();
     });
