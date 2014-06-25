83fb5bed4dfcb7072ed3a6fef6b5d042cb311cd6
Kuba Kuźma <kuba@jah.pl>
2014-06-25 12:06:15 +0200

    watch on functions

--- app/assets/javascripts/controllers/claim_edit_claim.js
+++ app/assets/javascripts/controllers/claim_edit_claim.js
@@ -10,26 +10,26 @@ angular.module("moBilling.controllers.claimEditClaim", [])
         });
 
         // first_seen_consult
-        $scope.$watchGroup(["claim.admission_on", "claim.first_seen_on"], function () {
-            if (!$scope.isFirstSeenConsultVisible()) {
-                $scope.claim.first_seen_consult = true;
-            }
-        });
-
         $scope.isFirstSeenConsultVisible = function () {
             return $scope.claim.admission_on !== $scope.claim.first_seen_on;
         };
 
-        // icu_transfer
-        $scope.$watchGroup(["claim.admission_on", "claim.first_seen_on", "claim.most_responsible_physician"], function () {
-            if (!$scope.isICUTransferVisible()) {
-                $scope.claim.icu_transfer = false;
+        $scope.$watch($scope.isFirstSeenConsultVisible, function (isFirstSeenConsultVisible) {
+            if (!isFirstSeenConsultVisible) {
+                $scope.claim.first_seen_consult = true;
             }
         });
 
+        // icu_transfer
         $scope.isICUTransferVisible = function () {
             return $scope.isFirstSeenConsultVisible() && $scope.claim.most_responsible_physician;
         };
+
+        $scope.$watch($scope.isICUTransferVisible, function (isICUTransferVisible) {
+            if (!isICUTransferVisible) {
+                $scope.claim.icu_transfer = false;
+            }
+        });
 
         $scope.$watch("claim.photo_id", function (photo_id) {
             if (photo_id) {
--- app/assets/javascripts/controllers/claim_edit_consult.js
+++ app/assets/javascripts/controllers/claim_edit_consult.js
@@ -1,14 +1,14 @@
 angular.module("moBilling.controllers.claimEditConsult", [])
 
     .controller("ClaimEditConsultController", function ($scope) {
-        $scope.$watch("claim.consult_type", function (consult_type) {
-            if (!$scope.isTimeVisible()) {
+        $scope.isTimeVisible = function () {
+            return $scope.claim.consult_type && $scope.claim.consult_type.indexOf("comprehensive") === 0;
+        };
+
+        $scope.$watch($scope.isTimeVisible, function (isTimeVisible) {
+            if (!isTimeVisible) {
                 $scope.claim.consult_time_in = undefined;
                 $scope.claim.consult_time_out = undefined;
             }
         });
-
-        $scope.isTimeVisible = function () {
-            return $scope.claim.consult_type && $scope.claim.consult_type.indexOf("comprehensive") === 0;
-        };
     });
