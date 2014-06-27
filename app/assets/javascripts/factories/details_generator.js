d5674ab9495198102a83b5a5627164d170905bf1
Kuba Kuźma <kuba@jah.pl>
2014-06-27 11:00:59 +0200

    added generator

--- app/assets/javascripts/factories/details_generator.js
+++ app/assets/javascripts/factories/details_generator.js
@@ -1,6 +1,22 @@
 angular.module("moBilling.factories.detailsGenerator", [])
 
     .factory("detailsGenerator", function () {
+        function daysRange(first, last) {
+            var i,
+                days = [];
+
+            first = new Date(first).getTime();
+            last = new Date(last).getTime();
+
+            if (!isNaN(first) && !isNaN(last)) {
+                for (i = first; i <= last; i += 1000 * 60 * 60 * 24) {
+                    days.push(new Date(i).toISOString().substr(0, 10));
+                }
+            }
+
+            return days;
+        }
+
         function consultCode(type) {
             return {
                 general_er:           "A135",
@@ -52,97 +68,106 @@ angular.module("moBilling.factories.detailsGenerator", [])
             }[(er ? "er_" : "non_er_") + type];
         }
 
-        function daysRange(first, last) {
-            var i,
-                days = [];
-
-            first = new Date(first).getTime();
-            last = new Date(last).getTime();
-
-            if (!isNaN(first) && !isNaN(last)) {
-                for (i = first; i <= last; i += 1000 * 60 * 60 * 24) {
-                    days.push(new Date(i).toISOString().substr(0, 10));
-                }
-            }
-
-            return days;
-        }
-
-        function first(days, mrp, admission, discharge) {
-            var details = [];
-
-            days.forEach(function (day, i) {
-                if (day === 0) {
-                    if (mrp && admission) {
-                        details.push({ day: day, code: "E082" }); // admission premium
-                    }
-                } else {
-                    if (i === days.length - 1 && discharge) {
-                        if (mrp) {
-                            details.push({ day: day, code: "C124" });
-                            details.push({ day: day, code: "E083" });
-                        }
-                    } else {
-                        if (day === 1) {
-                            if (mrp) {
-                                details.push({ day: day, code: "C122" });
-                                details.push({ day: day, code: "E083" });
-                            }
-                        } else if (day === 2) {
-                            if (mrp) {
-                                details.push({ day: day, code: "C123" });
-                                details.push({ day: day, code: "E083" });
-                            }
-                        } else if (day >= 3 && day <= 35) {
-                            details.push({ day: day, code: "C132" });
-                            if (mrp) {
-                                details.push({ day: day, code: "E083" });
-                            }
-                        } else if (day >= 36 && day <= 91) {
-                            details.push({ day: day, code: "C137" });
-                            if (mrp) {
-                                details.push({ day: day, code: "E083" });
-                            }
-                        } else if (day >= 92) {
-                            details.push({ day: day, code: "C139" });
-                            if (mrp) {
-                                details.push({ day: day, code: "E083" });
-                            }
-                        }
-                    }
-                }
-            });
-
-            return details;
-        }
-
-        function second(days) {
-            var details = [];
-
-            days.forEach(function (day, i) {
-                if (day >= 1) {
-                    details.push({ day: day, code: "C138" });
-                }
-            });
-
-            return details;
-        }
-
         function detailsGenerator(claim) {
             var first = claim.first_seen_on,
                 last = claim.last_seen_on,
                 admission = claim.admission_on === claim.first_seen_on,
                 mrp = claim.most_responsible_physician,
                 consult = claim.consult_type,
+                visit = claim.consult_premium_visit,
+                travel = claim.consult_premium_travel,
                 er = /^(general|comprehensive|limited)_er$/.test(claim.consult_type),
                 icu = claim.icu_transfer,
                 discharge = claim.last_seen_discharge,
-                days = [],
-                codes = [];
+                details = [];
 
-            return daysRange(first, last).map(function (day) {
-                return { day: day };
+            daysRange(first, last).forEach(function (day, i) {
+                if (day === first) {
+                    if (consult) {
+                        // consult
+                        details.push({ day: day, code: consultCode(consult) });
+
+                        if (visit) {
+                            // visit premium
+                            details.push({ day: day, code: premiumVisitCode(true, er, visit) });
+
+                            if (travel) {
+                                // travel premium
+                                details.push({ day: day, code: premiumTravelCode(er, visit) });
+                            }
+                        }
+
+                        if (admission && mrp) {
+                            // admission premium
+                            details.push({ day: day, code: "E082" });
+                        }
+                    }
+                } else if (day === last && discharge) {
+                    // discharge
+                    if (mrp) {
+                        details.push({ day: day, code: "C124" });
+                        details.push({ day: day, code: "E083" });
+                    } else {
+                        details.push({ day: day, code: "C138" });
+                    }
+                } else if (i === 1) {
+                    // first day after admission
+                    if (mrp) {
+                        if (icu) {
+                            details.push({ day: day, code: "C142" });
+                            details.push({ day: day, code: "E083" });
+                        } else {
+                            details.push({ day: day, code: "C122" });
+                            details.push({ day: day, code: "E083" });
+                        }
+                    } else {
+                        details.push({ day: day, code: "C138" });
+                    }
+                } else if (i === 2) {
+                    // second day after admission
+                    if (mrp) {
+                        if (icu) {
+                            details.push({ day: day, code: "C143" });
+                            details.push({ day: day, code: "E083" });
+                        } else {
+                            details.push({ day: day, code: "C123" });
+                            details.push({ day: day, code: "E083" });
+                        }
+                    } else {
+                        details.push({ day: day, code: "C138" });
+                    }
+                } else if (i >= 3 && i <= 35) {
+                    // 3-35 day after admission
+                    if (mrp) {
+                        details.push({ day: day, code: "C132" });
+                        details.push({ day: day, code: "E083" });
+                    } else {
+                        details.push({ day: day, code: "C138" });
+                    }
+                } else if (i >= 36 && i <= 91) {
+                    // 36-91 day after admission
+                    if (mrp) {
+                        details.push({ day: day, code: "C137" });
+                        details.push({ day: day, code: "E083" });
+                    } else {
+                        details.push({ day: day, code: "C138" });
+                    }
+                } else if (i >= 92) {
+                    // 92+ day after admission
+                    if (mrp) {
+                        details.push({ day: day, code: "C139" });
+                        details.push({ day: day, code: "E083" });
+                    } else {
+                        details.push({ day: day, code: "C138" });
+                    }
+                }
             });
+
+            details.forEach(function (detail) {
+                detail.autogenerated = true;
+            });
+
+            return details;
         }
 
         return detailsGenerator;
