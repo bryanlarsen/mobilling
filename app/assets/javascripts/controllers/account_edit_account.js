1ff928a9ac9bebb528f665be6e7692c64f417a70
redCashion <xperts@gmail.com>
2014-07-20 03:13:02 -0400

    add save action in angular

--- app/assets/javascripts/controllers/account_edit_account.js
+++ app/assets/javascripts/controllers/account_edit_account.js
@@ -1,5 +1,15 @@
 angular.module("moBilling.controllers.accountEdit", [])
 
-    .controller("AccountEditAccountController", function ($scope) {
+  .controller("AccountEditAccountController", function ($scope, User) {
+    $scope.user = User.get();
 
-    });
+    $scope.save = function () {
+      $scope.submitting = true;
+      $scope.user.status = "saved";
+      User.save($scope.user, function () {
+          console.log("saved");
+      }, function () {
+          $scope.submitting = false;
+      });
+    };
+  });
