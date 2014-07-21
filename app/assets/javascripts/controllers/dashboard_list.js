50ea6bd5095f311657b33da3d0f4fb4cec107b7c
redCashion <xperts@gmail.com>
2014-07-21 07:18:20 -0400

    add styling for sort table

--- app/assets/javascripts/controllers/dashboard_list.js
+++ app/assets/javascripts/controllers/dashboard_list.js
@@ -1,4 +1,6 @@
 angular.module("moBilling.controllers.dashboardList", [])
   .controller("DashboardListController", function ($scope, doctors, Doctor) {
     $scope.doctors = doctors;
+    $scope.predicate = '-name';
+    $scope.reverse = false;
   });
