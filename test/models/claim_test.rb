03d7b462e2720e4a7a63e4445c36132f75e39aa7
redCashion <xperts@gmail.com>
2014-07-22 04:22:47 -0400

    add unit tests

--- test/models/claim_test.rb
+++ test/models/claim_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class ClaimTest < ActiveSupport::TestCase
   setup do
+    @user = create(:user)
     @claim = build(:claim)
   end
 
@@ -23,5 +24,47 @@ class ClaimTest < ActiveSupport::TestCase
     assert Claim.submitted.include?(rejected_admin_attention_claim)
     assert Claim.submitted.include?(rejected_doctor_attention_claim)
     assert Claim.submitted.include?(paid_claim)
+  end
+
+  test "a saved claim should be result in saved_claim_count tally" do
+    saved_claim = create(:claim, :saved)
+    assert @user.saved_count == 0
+    @user.claims.push(saved_claim)
+    assert @user.saved_count == 1
+  end
+
+  test "a unprocessed claim should be result in unprocessed_claim_count tally" do
+    unprocessed_claim = create(:claim, :unprocessed)
+    assert @user.unprocessed_count == 0
+    @user.claims.push(unprocessed_claim)
+    assert @user.unprocessed_count == 1
+  end
+
+  test "a processed claim should be result in processed_claim_count tally" do
+    processed_claim = create(:claim, :processed)
+    assert @user.processed_count == 0
+    @user.claims.push(processed_claim)
+    assert @user.processed_count == 1
+  end
+
+  test "a rejected_admin_attention claim should be result in rejected_admin_attention_claim_count tally" do
+    rejected_admin_attention_claim = create(:claim, :rejected_admin_attention)
+    assert @user.rejected_admin_attention_count == 0
+    @user.claims.push(rejected_admin_attention_claim)
+    assert @user.rejected_admin_attention_count == 1
+  end
+
+  test "a rejected_doctor_attention claim should be result in rejected_doctor_attention_claim_count tally" do
+    rejected_doctor_attention_claim = create(:claim, :rejected_doctor_attention)
+    assert @user.rejected_doctor_attention_count == 0
+    @user.claims.push(rejected_doctor_attention_claim)
+    assert @user.rejected_doctor_attention_count == 1
+  end
+
+  test "a paid claim should be result in paid_claim_count tally" do
+    paid_claim = create(:claim, :paid)
+    assert @user.paid_count == 0
+    @user.claims.push(paid_claim)
+    assert @user.paid_count == 1
   end
 end
