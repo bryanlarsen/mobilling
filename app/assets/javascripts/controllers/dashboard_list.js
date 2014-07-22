95c2e914d642014f3082f9f3dad011358482f76a
redCashion <xperts@gmail.com>
2014-07-22 03:07:15 -0400

    add sort columns

--- app/assets/javascripts/controllers/dashboard_list.js
+++ app/assets/javascripts/controllers/dashboard_list.js
@@ -1,6 +1,11 @@
 angular.module("moBilling.controllers.dashboardList", [])
   .controller("DashboardListController", function ($scope, doctors, Doctor) {
     $scope.doctors = doctors;
-    $scope.predicate = '-name';
+    $scope.predicate = 'name';
     $scope.reverse = false;
+
+    $scope.isCurrent = function(column) {
+      return column == $scope.predicate;
+    };
+
   });
