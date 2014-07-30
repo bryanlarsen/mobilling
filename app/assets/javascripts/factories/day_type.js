b8f75bf3ea0e6ff3f2ff5d037c8ae445595f5116
Kuba Kuźma <kuba@jah.pl>
2014-07-30 19:38:10 +0200

    using UTC in dates

--- app/assets/javascripts/factories/day_type.js
+++ app/assets/javascripts/factories/day_type.js
@@ -24,7 +24,7 @@ angular.module("moBilling.factories.dayType", [])
         function easter(string) {
             var c, n, k, i, j, l, month, day, year;
 
-            year = parseDate(string).getFullYear();
+            year = parseDate(string).getUTCFullYear();
 
             c = Math.floor(year / 100);
             n = year - 19 * Math.floor(year / 19);
@@ -46,9 +46,9 @@ angular.module("moBilling.factories.dayType", [])
             var date, day, year, month;
 
             date = parseDate(string);
-            year = date.getFullYear();
-            month = date.getMonth();
-            day = parseDate(year, month, 1).getDay();
+            year = date.getUTCFullYear();
+            month = date.getUTCMonth();
+            day = parseDate(year, month, 1).getUTCDay();
 
             return parseDate(year, month, 1 + ((7 - day + 1) % 7));
         }
@@ -57,90 +57,90 @@ angular.module("moBilling.factories.dayType", [])
             var holiday = parseDate(easter(string) - 1000 * 60 * 60 * 24 * 2),
                 date = parseDate(string);
 
-            return date.getFullYear() === holiday.getFullYear() && date.getMonth() === holiday.getMonth() && date.getDate() === holiday.getDate();
+            return date.getUTCFullYear() === holiday.getUTCFullYear() && date.getUTCMonth() === holiday.getUTCMonth() && date.getUTCDate() === holiday.getUTCDate();
         }
 
         function boxingDay(string) {
             var date = parseDate(string);
 
-            return date.getMonth() === 11 && date.getDate() === 26;
+            return date.getUTCMonth() === 11 && date.getUTCDate() === 26;
         }
 
         function canadaDay(string) {
             var holiday,
                 date = parseDate(string),
-                julyFirst = parseDate(date.getFullYear(), 6, 1),
-                julySecond = parseDate(date.getFullYear(), 6, 2);
+                julyFirst = parseDate(date.getUTCFullYear(), 6, 1),
+                julySecond = parseDate(date.getUTCFullYear(), 6, 2);
 
-            if (julyFirst.getDay() === 0) {
+            if (julyFirst.getUTCDay() === 0) {
                 holiday = julySecond;
             } else {
                 holiday = julyFirst;
             }
 
-            return date.getFullYear() === holiday.getFullYear() && date.getMonth() === holiday.getMonth() && date.getDate() === holiday.getDate();
+            return date.getUTCFullYear() === holiday.getUTCFullYear() && date.getUTCMonth() === holiday.getUTCMonth() && date.getUTCDate() === holiday.getUTCDate();
         }
 
         function christmas(string) {
             var date = parseDate(string);
 
-            return date.getMonth() === 11 && date.getDate() === 25;
+            return date.getUTCMonth() === 11 && date.getUTCDate() === 25;
         }
 
         function civicHoliday(string) {
             var holiday,
                 date = parseDate(string);
 
-            holiday = firstMonday(parseDate(date.getFullYear(), 7, 1));
+            holiday = firstMonday(parseDate(date.getUTCFullYear(), 7, 1));
 
-            return date.getFullYear() === holiday.getFullYear() && date.getMonth() === holiday.getMonth() && date.getDate() === holiday.getDate();
+            return date.getUTCFullYear() === holiday.getUTCFullYear() && date.getUTCMonth() === holiday.getUTCMonth() && date.getUTCDate() === holiday.getUTCDate();
         }
 
         function labourDay(string) {
             var holiday,
                 date = parseDate(string);
 
-            holiday = firstMonday(parseDate(date.getFullYear(), 8, 1));
+            holiday = firstMonday(parseDate(date.getUTCFullYear(), 8, 1));
 
-            return date.getFullYear() === holiday.getFullYear() && date.getMonth() === holiday.getMonth() && date.getDate() === holiday.getDate();
+            return date.getUTCFullYear() === holiday.getUTCFullYear() && date.getUTCMonth() === holiday.getUTCMonth() && date.getUTCDate() === holiday.getUTCDate();
         }
 
         function newYearsDay(string) {
             var date = parseDate(string);
 
-            return date.getMonth() === 0 && date.getDate() === 1;
+            return date.getUTCMonth() === 0 && date.getUTCDate() === 1;
         }
 
         function thanksgiving(string) {
             var holiday,
                 date = parseDate(string);
 
-            holiday = parseDate(firstMonday(new Date(date.getFullYear(), 9, 1)).getTime() + 1000 * 60 * 60 * 24 * 7);
+            holiday = parseDate(firstMonday(parseDate(date.getUTCFullYear(), 9, 1)).getTime() + 1000 * 60 * 60 * 24 * 7);
 
-            return date.getFullYear() === holiday.getFullYear() && date.getMonth() === holiday.getMonth() && date.getDate() === holiday.getDate();
+            return date.getUTCFullYear() === holiday.getUTCFullYear() && date.getUTCMonth() === holiday.getUTCMonth() && date.getUTCDate() === holiday.getUTCDate();
         }
 
         function victoriaDay(string) {
             var year, holiday, mayTwentyFourth,
                 date = parseDate(string);
 
-            year = date.getFullYear();
+            year = date.getUTCFullYear();
 
             mayTwentyFourth = parseDate(year, 4, 24);
 
-            if (mayTwentyFourth.getDay() > 1) {
-                holiday = parseDate(mayTwentyFourth.getTime() - (mayTwentyFourth.getDay() - 1) * 1000 * 60 * 60 * 24);
-            } else if (mayTwentyFourth.getDay() === 0) {
+            if (mayTwentyFourth.getUTCDay() > 1) {
+                holiday = parseDate(mayTwentyFourth.getTime() - (mayTwentyFourth.getUTCDay() - 1) * 1000 * 60 * 60 * 24);
+            } else if (mayTwentyFourth.getUTCDay() === 0) {
                 holiday = parseDate(mayTwentyFourth.getTime() - 1000 * 60 * 60 * 24 * 6);
             }
 
-            return date.getFullYear() === holiday.getFullYear() && date.getMonth() === holiday.getMonth() && date.getDate() === holiday.getDate();
+            return date.getUTCFullYear() === holiday.getUTCFullYear() && date.getUTCMonth() === holiday.getUTCMonth() && date.getUTCDate() === holiday.getUTCDate();
         }
 
         function weekend(string) {
             var date = parseDate(string);
 
-            return date.getDay() === 0 || date.getDay() === 6;
+            return date.getUTCDay() === 0 || date.getUTCDay() === 6;
         }
 
         function dayType(string) {
