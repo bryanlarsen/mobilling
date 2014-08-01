e1490c5c4a02bf0d2f7abd9e0ceb0b30570a3183
Kuba Kuźma <kuba@jah.pl>
2014-08-01 11:50:19 +0200

    fixed signin in

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,10 +1,11 @@
 class Admin::DashboardsController < Admin::ApplicationController
-  # include Sortable
+  include Sortable
 
-  # self.sortable_columns = User::Dashboard::SORTABLE_COLUMNS
+  self.sortable_columns = ["name"] # User::Dashboard::SORTABLE_COLUMNS
 
   def show
     authorize :dashboard, :read?
     # @users = User.accessible_by(current_ability).dashboard.order("#{sort_column} #{sort_direction}")
+    @users = ::User.all
   end
 end
--- app/views/v1/users/_user.json.jbuilder
+++ app/views/v1/users/_user.json.jbuilder
@@ -1,5 +1,4 @@
 json.id                   user.id
 json.name                 user.name
 json.email                user.email
-json.agent_id             user.agent_id
 json.authentication_token user.authentication_token
