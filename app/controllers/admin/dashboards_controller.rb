a441148dd7ac5656323f3fe4e4f31111b36dde34
Kuba Kuźma <kuba@jah.pl>
2014-08-01 11:55:29 +0200

    added dashboard scope

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,11 +1,11 @@
 class Admin::DashboardsController < Admin::ApplicationController
   include Sortable
 
-  self.sortable_columns = ["name"] # User::Dashboard::SORTABLE_COLUMNS
+  self.sortable_columns = %w[name saved_count unprocessed_count processed_count rejected_admin_attention_count rejected_doctor_attention_count paid_count]
 
   def show
     authorize :dashboard, :read?
     # @users = User.accessible_by(current_ability).dashboard.order("#{sort_column} #{sort_direction}")
-    @users = ::User.all
+    @users = ::User.dashboard.order("#{sort_column} #{sort_direction}")
   end
 end
