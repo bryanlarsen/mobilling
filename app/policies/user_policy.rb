211d753fcd838aab3f2d57c18d20b92cb85a7704
Kuba Kuźma <kuba@jah.pl>
2014-08-04 18:50:12 +0200

    removed restriction for admins

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -2,7 +2,7 @@ class UserPolicy < Struct.new(:current_user, :user)
   class Scope < Struct.new(:current_user, :scope)
     def resolve
       scope = ::User.includes(:agent)
-      scope = scope.where(admin_users: {id: current_user.id})
+      scope = scope.where(admin_users: {id: current_user.id}) if current_user.agent?
       scope
     end
   end
