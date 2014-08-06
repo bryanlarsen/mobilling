2a8ff9c92a77f5638a2b771fcf8949eb04295a78
Kuba Kuźma <kuba@jah.pl>
2014-08-06 12:23:37 +0200

    updated apipie

--- test/controllers/v1/photos_controller_test.rb
+++ test/controllers/v1/photos_controller_test.rb
@@ -1,6 +1,26 @@
 require "test_helper"
 
 class V1::PhotosControllerTest < ActionController::TestCase
+  test "show renders template" do
+    user = create(:user, :authenticated)
+    photo = create(:photo, user: user)
+    get :show, id: photo.id, auth: user.authentication_token, format: "json"
+    assert_template "show"
+  end
+
+  test "show responds with unauthorized" do
+    photo = create(:photo)
+    get :show, id: photo.id, format: "json"
+    assert_response :unauthorized
+  end
+
+  test "show responds with not found" do
+    user = create(:user, :authenticated)
+    photo = create(:photo)
+    get :show, id: photo.id, auth: user.authentication_token, format: "json"
+    assert_response :not_found
+  end
+
   test "create responds with created" do
     user = create(:user, :authenticated)
     post :create, photo: {file: Rack::Test::UploadedFile.new(file_fixture("image.png")) }, auth: user.authentication_token, format: "json"
--- test/controllers/v1/users_controller_test.rb
+++ test/controllers/v1/users_controller_test.rb
@@ -1,6 +1,17 @@
 require "test_helper"
 
 class V1::UsersControllerTest < ActionController::TestCase
+  test "show renders template" do
+    user = create(:user, :authenticated)
+    get :show, auth: user.authentication_token, format: "json"
+    assert_template "show"
+  end
+
+  test "show responds with unauthorized" do
+    get :show, format: "json"
+    assert_response :unauthorized
+  end
+
   test "create responds with created" do
     agent = create(:admin_user, role: "agent")
     post :create, user: attributes_for(:user).merge(agent_id: agent.id), format: "json"
@@ -16,5 +27,16 @@ class V1::UsersControllerTest < ActionController::TestCase
     user = create(:user, :authenticated)
     put :update, auth: user.authentication_token, format: "json", user: {name: "Bob"}
     assert_template "update"
+  end
+
+  test "update responds with unprocessable entity" do
+    user = create(:user, :authenticated)
+    post :update, auth: user.authentication_token, user: {name: ""}, format: "json"
+    assert_response :unprocessable_entity
+  end
+
+  test "update responds with unauthorized" do
+    post :update, user: {name: ""}, format: "json"
+    assert_response :unauthorized
   end
 end
