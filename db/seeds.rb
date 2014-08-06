b050f3f3496b435269025403de0ca1dc5926dc10
Kuba Kuźma <kuba@jah.pl>
2014-08-06 19:47:20 +0200

    added admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,34 @@
-ActiveRecord::Migration.say_with_time "create_admin" do
-  Admin::User.create(name: "Admin", email: "admin@example.com", password: "secret", role: "admin")
-end
+# ActiveRecord::Migration.say_with_time "create_admin" do
+#   Admin::User.create(name: "Admin", email: "admin@example.com", password: "secret", role: "admin")
+# end
 
-ActiveRecord::Migration.say_with_time "create_agent" do
-  Admin::User.create(name: "Agent", email: "agent@example.com", password: "secret", role: "agent")
-end
+# ActiveRecord::Migration.say_with_time "create_agent" do
+#   Admin::User.create(name: "Agent", email: "agent@example.com", password: "secret", role: "agent")
+# end
 
-ActiveRecord::Migration.say_with_time "create_hospitals" do
-  Hospital.destroy_all
-  hospitals = open(Rails.root.join("db/seeds/hospitals.txt")).readlines.map { |l| {name: l.strip} }
-  Hospital.create(hospitals)
-end
+# ActiveRecord::Migration.say_with_time "create_hospitals" do
+#   Hospital.destroy_all
+#   hospitals = open(Rails.root.join("db/seeds/hospitals.txt")).readlines.map { |l| {name: l.strip} }
+#   Hospital.create(hospitals)
+# end
 
-ActiveRecord::Migration.say_with_time "create_diagnoses" do
-  Diagnosis.destroy_all
-  diagnoses = open(Rails.root.join("db/seeds/diagnoses.txt")).readlines.map { |l| {name: l.strip} }
-  Diagnosis.create(diagnoses)
-end
+# ActiveRecord::Migration.say_with_time "create_diagnoses" do
+#   Diagnosis.destroy_all
+#   diagnoses = open(Rails.root.join("db/seeds/diagnoses.txt")).readlines.map { |l| {name: l.strip} }
+#   Diagnosis.create(diagnoses)
+# end
 
 ActiveRecord::Migration.say_with_time "create_service_codes" do
   ServiceCode.destroy_all
   service_codes = open(Rails.root.join("db/seeds/service_codes.txt")).readlines.map do |line|
-    code = line[-6..-2].strip
-    desc = line[0..-9].strip
-    {name: "#{code} #{desc}"}
+    match = line.strip.match(/\A(.*)\((.*)\)\Z/)
+    code = match[2].strip
+    desc = match[1].strip
+    puts line if desc.blank?
+    # code = line.strip[-6..-2].strip
+    # desc = line.strip[0..-9].strip
+    # puts "#{code} #{desc}"
+    {name: "#{code} #{desc}".strip}
   end
-  ServiceCode.create(service_codes)
+  # ServiceCode.create(service_codes)
 end
