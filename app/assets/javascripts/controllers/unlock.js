732a0b47df769b1984133c53cf74957344982bc2
Kuba Kuźma <kuba@jah.pl>
2014-08-07 18:18:35 +0200

    added unlock screen

--- app/assets/javascripts/controllers/unlock.js
+++ app/assets/javascripts/controllers/unlock.js
@@ -1,13 +1,42 @@
 angular.module("moBilling.controllers.unlock", [])
 
     .controller("UnlockController", function ($scope, $location, user) {
+        $scope.retries = 3;
         $scope.unlock = {};
+
+        if (!user.pin) {
+            success();
+        }
+
+        function success() {
+            $location.path("/claims/saved").hash("").replace();
+        };
+
+        function error() {
+            $scope.retries--;
+
+            if ($scope.retries === 0) {
+                window.localStorage.removeItem("authenticationToken");
+                $location.path("/").hash("").replace();
+            } else {
+                $scope.submitting = false;
+                $scope.errors = { pin: ["is invaild"] };
+                $scope.form.pin.$setValidity("server", false);
+            }
+        };
 
         $scope.submit = function () {
             $scope.submitted = true;
 
             if ($scope.form.$valid) {
-                console.log($scope.unlock.pin);
+                $scope.submitting = true;
+                if ($scope.unlock.pin === user.pin) {
+                    success();
+                } else {
+                    window.setTimeout(function () {
+                        $scope.$apply(error);
+                    }, 3000);
+                }
             }
         };
     });
