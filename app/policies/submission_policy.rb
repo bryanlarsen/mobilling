030160bfa51bbf1c1d4cf5116039ca62bef9ba96
Bryan Larsen <bryan@larsen.st>
2014-08-13 14:48:58 -0400

    I have no idea why this didn't work before, but it's working now, and allows agents to access the submissions page

--- app/policies/submission_policy.rb
+++ app/policies/submission_policy.rb
@@ -2,7 +2,7 @@ class SubmissionPolicy < Struct.new(:current_user, :user)
   class Scope < Struct.new(:current_user, :scope)
     def resolve
       return ::Submission.none if current_user.blank?
-      scope = ::EdtFile.where(type: 'Submission').includes(:user)
+      scope = ::Submission.includes(:user)
       scope = scope.where(users: {agent_id: current_user.id}) if current_user.agent?
       scope
     end
