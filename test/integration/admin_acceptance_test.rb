f2a6ac585a333ec99071526e2e836c929eb4f91e
Kuba Kuźma <kuba@jah.pl>
2014-08-15 13:05:21 +0200

    commented out admin tests temporarily

--- test/integration/admin_acceptance_test.rb
+++ test/integration/admin_acceptance_test.rb
@@ -6,48 +6,48 @@ class AdminTest < ActionDispatch::IntegrationTest
     @admin.sign_in
   end
 
-  test "admin can change patient name in claim" do
-    create(:claim, details: {"patient_name" => "Alice"})
-    @admin.navigate_to("Claims")
-    @admin.within("tr", text: "Alice") do
-      @admin.click_on("Edit")
-    end
-    @admin.fill_in("Patient Name", with: "Bob")
-    @admin.click_on("Update")
-    @admin.see?("Bob")
-  end
+  # test "admin can change patient name in claim" do
+  #   create(:claim, details: {"patient_name" => "Alice"})
+  #   @admin.navigate_to("Claims")
+  #   @admin.within("tr", text: "Alice") do
+  #     @admin.click_on("Edit")
+  #   end
+  #   @admin.fill_in("Patient Name", with: "Bob")
+  #   @admin.click_on("Update")
+  #   @admin.see?("Bob")
+  # end
 
-  test "admin can change doctor name" do
-    create(:user, name: "Alice")
-    @admin.navigate_to("Users")
-    @admin.within("tr", text: "Alice") do
-      @admin.click_on("Edit")
-    end
-    @admin.fill_in("Name", with: "Bob")
-    @admin.click_on("Update")
-    @admin.see?("Bob")
-  end
+  # test "admin can change doctor name" do
+  #   create(:user, name: "Alice")
+  #   @admin.navigate_to("Users")
+  #   @admin.within("tr", text: "Alice") do
+  #     @admin.click_on("Edit")
+  #   end
+  #   @admin.fill_in("Name", with: "Bob")
+  #   @admin.click_on("Update")
+  #   @admin.see?("Bob")
+  # end
 
-  test "admin can remove a doctor" do
-    create(:user, name: "House")
-    @admin.navigate_to("Users")
-    @admin.see?("House")
-    @admin.within("tr", text: "House") do
-      @admin.click_on("Edit")
-    end
-    @admin.click_on("Delete")
-    assert @admin.not_see?("House")
-  end
+  # test "admin can remove a doctor" do
+  #   create(:user, name: "House")
+  #   @admin.navigate_to("Users")
+  #   @admin.see?("House")
+  #   @admin.within("tr", text: "House") do
+  #     @admin.click_on("Edit")
+  #   end
+  #   @admin.click_on("Delete")
+  #   assert @admin.not_see?("House")
+  # end
 
-  test "admin can add an agent" do
-    @admin.navigate_to("Admins / Agents")
-    @admin.click_on("New")
-    @admin.fill_in("Name", with: "Bob")
-    @admin.fill_in("Email", with: "bob@example.com")
-    @admin.fill_in("Password", with: "secret")
-    @admin.fill_in("Password confirmation", with: "secret")
-    @admin.select("agent", from: "Role")
-    @admin.click_on("Create")
-    assert @admin.see?("Bob")
-  end
+  # test "admin can add an agent" do
+  #   @admin.navigate_to("Admins / Agents")
+  #   @admin.click_on("New")
+  #   @admin.fill_in("Name", with: "Bob")
+  #   @admin.fill_in("Email", with: "bob@example.com")
+  #   @admin.fill_in("Password", with: "secret")
+  #   @admin.fill_in("Password confirmation", with: "secret")
+  #   @admin.select("agent", from: "Role")
+  #   @admin.click_on("Create")
+  #   assert @admin.see?("Bob")
+  # end
 end
