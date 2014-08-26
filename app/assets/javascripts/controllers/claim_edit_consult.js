7029d0aa55aab8c305275b69dffb2f3cff7cdd54
Kuba Kuźma <kuba@jah.pl>
2014-08-26 10:52:58 +0200

    added off-value

--- app/assets/javascripts/controllers/claim_edit_consult.js
+++ app/assets/javascripts/controllers/claim_edit_consult.js
@@ -67,47 +67,35 @@ angular.module("moBilling.controllers")
 
         $scope.consultTypes = {
             internal_medicine: ["general", "comprehensive", "limited"],
-            cardiology: ["general", "comprehensive", "limited"],
-            family_medicine: ["general", "special", "comprehensive", "on_call_admission"]
+            cardiology:        ["general", "comprehensive", "limited"],
+            family_medicine:   ["general", "special", "comprehensive", "on_call_admission"]
         }[$scope.claim.specialty];
 
         $scope.isConsultTimeVisible = function () {
             return ["comprehensive_er", "comprehensive_non_er", "special_er", "special_non_er"].indexOf($scope.claim.consult_type) !== -1;
         };
 
-        function consultPremiumVisitLimit(consultPremiumVisit) {
-            if (consultPremiumVisit === undefined) {
-                consultPremiumVisit = $scope.claim.consult_premium_visit;
-            }
+        $scope.consultPremiumVisits = [
+            "weekday_day",
+            "weekday_office_hours",
+            "weekday_evening",
+            "weekday_night",
+            "weekend_day",
+            "weekend_night",
+            "holiday_day",
+            "holiday_night"
+        ];
 
-            return {
-                weekday_day:          10,
-                weekday_office_hours: 10,
-                weekday_evening:      10,
-                weekday_night:        Infinity,
-                weekend_day:          20,
-                weekend_night:        Infinity,
-                holiday_day:          20,
-                holiday_night:        Infinity
-            }[consultPremiumVisit];
-        }
-
-        function consultPremiumTravelLimit(consultPremiumVisit) {
-            if (consultPremiumVisit === undefined) {
-                consultPremiumVisit = $scope.claim.consult_premium_visit;
-            }
-
-            return {
-                weekday_day:          2,
-                weekday_office_hours: 2,
-                weekday_evening:      2,
-                weekday_night:        Infinity,
-                weekend_day:          6,
-                weekend_night:        Infinity,
-                holiday_day:          6,
-                holiday_night:        Infinity
-            }[consultPremiumVisit];
-        }
+        $scope.consultPremiumVisitLabels = {
+            weekday_day:          "Day 7:00-17:00",
+            weekday_office_hours: "Office hours 7:00-17:00",
+            weekday_evening:      "Evening 17:00-0:00",
+            weekday_night:        "Night 0:00-7:00",
+            weekend_day:          "Weekend 7:00-0:00",
+            weekend_night:        "Night 0:00-7:00",
+            holiday_day:          "Holiday 7:00-0:00",
+            holiday_night:        "Night 0:00-7:00"
+        };
 
         $scope.$watchGroup([
             "isPremiumVisitVisible",
@@ -125,16 +113,7 @@ angular.module("moBilling.controllers")
                     return claim.id !== $scope.claim.id && claim.first_seen_on === $scope.claim.first_seen_on;
                 });
 
-                [
-                    "weekday_day",
-                    "weekday_office_hours",
-                    "weekday_evening",
-                    "weekday_night",
-                    "weekend_day",
-                    "weekend_night",
-                    "holiday_day",
-                    "holiday_night"
-                ].forEach(function (consult_premium_visit) {
+                $scope.consultPremiumVisits.forEach(function (consult_premium_visit) {
                     var consultPremiumVisitClaims = $filter("filter")(others, { consult_premium_visit: consult_premium_visit });
 
                     $scope.consultPremiumVisitCounts[consult_premium_visit] = consultPremiumVisitClaims.length;
@@ -154,14 +133,38 @@ angular.module("moBilling.controllers")
         });
 
         $scope.isConsultPremiumVisitLimitReached = function () {
-            var consult_premium_visit = $scope.claim.consult_premium_visit;
+            var limit,
+                consult_premium_visit = $scope.claim.consult_premium_visit;
 
-            return $scope.consultPremiumVisitCounts[consult_premium_visit] >= consultPremiumVisitLimit(consult_premium_visit);
+            limit = {
+                weekday_day:          10,
+                weekday_office_hours: 10,
+                weekday_evening:      10,
+                weekday_night:        Infinity,
+                weekend_day:          20,
+                weekend_night:        Infinity,
+                holiday_day:          20,
+                holiday_night:        Infinity
+            }[consult_premium_visit];
+
+            return $scope.consultPremiumVisitCounts[consult_premium_visit] >= limit;
         };
 
         $scope.isConsultPremiumTravelLimitReached = function () {
-            var consult_premium_visit = $scope.claim.consult_premium_visit;
+            var limit,
+                consult_premium_visit = $scope.claim.consult_premium_visit;
 
-            return $scope.consultPremiumTravelCounts[consult_premium_visit] >= consultPremiumTravelLimit(consult_premium_visit);
+            limit = {
+                weekday_day:          2,
+                weekday_office_hours: 2,
+                weekday_evening:      2,
+                weekday_night:        Infinity,
+                weekend_day:          6,
+                weekend_night:        Infinity,
+                holiday_day:          6,
+                holiday_night:        Infinity
+            }[consult_premium_visit];
+
+            return $scope.consultPremiumTravelCounts[consult_premium_visit] >= limit;
         };
     });
