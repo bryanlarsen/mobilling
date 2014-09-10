b5e5a57b48ab14bf7c74d1078d4e6647a981a1e7
Kuba Kuźma <kuba@jah.pl>
2014-09-10 14:26:39 +0200

    added ngannotate, configured compressors and uglifiers

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
-Rails.application.config.assets.precompile += %w[admin.css admin.js]
+Rails.application.config.assets.precompile += %w[*.png *.jpg *.otf *.eot *.svg *.ttf *.woff application.css application.js admin.css admin.js]
 
 if Rails.env.development?
   Rails.application.config.assets.precompile += %w[teaspoon.css teaspoon-teaspoon.js teaspoon-jasmine.js jasmine/1.3.1.js]
