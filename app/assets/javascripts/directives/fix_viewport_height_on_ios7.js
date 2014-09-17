0d9d61c0b9e02289db61de57866108e10ab270cb
Kuba Kuźma <kuba@jah.pl>
2014-09-17 09:51:56 +0200

    fixed?

--- app/assets/javascripts/directives/fix_viewport_height_on_ios7.js
+++ app/assets/javascripts/directives/fix_viewport_height_on_ios7.js
@@ -7,16 +7,11 @@ angular.module("moBilling.directives")
                 function fixViewportHeightOnIOS7() {
                     var height = Math.min($($window).height(), $window.innerHeight || Infinity);
 
-                    $("meta[name=viewport]").attr("content", "initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width");
-
                     element.height(height);
-                    element.parent().height(height);
                     $window.scrollTo(0, 0);
                 }
 
-                // $($window).on("resize orientationchange native.keyboardhide native.keyboardshow", fixViewportHeightOnIOS7);
-
-                $interval(fixViewportHeightOnIOS7, 1000);
+                $($window).on("resize orientationchange native.keyboardhide native.keyboardshow", fixViewportHeightOnIOS7);
 
                 fixViewportHeightOnIOS7();
             }
