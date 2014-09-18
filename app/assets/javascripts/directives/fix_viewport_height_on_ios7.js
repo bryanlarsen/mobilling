a6cbc9c3148fb6491af012f2ebb305681101be59
Kuba Kuźma <kuba@jah.pl>
2014-09-18 12:38:56 +0200

    testing fix #14

--- app/assets/javascripts/directives/fix_viewport_height_on_ios7.js
+++ app/assets/javascripts/directives/fix_viewport_height_on_ios7.js
@@ -14,7 +14,7 @@ angular.module("moBilling.directives")
                 }
 
                 if ($window.cordova && $window.cordova.plugins && $window.cordova.plugins.Keyboard) {
-                    $window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                    // $window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
                     $window.cordova.plugins.Keyboard.disableScroll(true);
                 }
 
