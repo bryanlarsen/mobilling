4428d580f8d1a3b14da50105798649e2686eb8ed
Kuba Kuźma <kuba@jah.pl>
2014-09-18 12:16:05 +0200

    reduced delay

--- app/assets/javascripts/directives/fix_viewport_height_on_ios7.js
+++ app/assets/javascripts/directives/fix_viewport_height_on_ios7.js
@@ -10,7 +10,7 @@ angular.module("moBilling.directives")
 
                         element.height(height);
                         $window.scrollTo(0, 0);
-                    }, 300);
+                    }, 100);
                 }
 
                 if ($window.cordova && $window.cordova.plugins && $window.cordova.plugins.Keyboard) {
