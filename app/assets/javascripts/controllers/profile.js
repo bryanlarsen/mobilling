000036318c02794aa55fe9a2907cfbb0ad9521dd
Bryan Larsen <bryan@larsen.st>
2014-09-25 16:07:39 -0400

    use a select for office code and specialty code

--- app/assets/javascripts/controllers/profile.js
+++ app/assets/javascripts/controllers/profile.js
@@ -5,6 +5,25 @@ angular.module("moBilling.controllers")
             $scope.agents = agents;
             $scope.specialties = specialties;
             $scope.user = user;
+
+            $scope.office_codes = [
+                { name: 'Hamilton (G)', code: 'G' },
+                { name: 'Kingston (J)', code: 'J' },
+                { name: 'London (P)', code: 'P' },
+                { name: 'Missisauga (E)', code: 'E' },
+                { name: 'Oshawa (F)', code: 'F' },
+                { name: 'Ottawa (D)', code: 'D' },
+                { name: 'Sudbury (R)', code: 'R' },
+                { name: 'Thunder Bay (U)', code: 'U' },
+                { name: 'Toronto (N)', code: 'N' }
+            ];
+
+            $scope.specialty_codes = [
+                { name: 'Family Medicine (00)', code: 0 },
+                { name: 'Anesthesia (01)', code: 1 },
+                { name: 'Internal Medicine (00)', code: 13 },
+                { name: 'Cardiology (60)', code: 60 }
+            ];
         };
 
         function success(user) {
