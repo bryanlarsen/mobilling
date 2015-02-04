d9a289b06452a24f45ecc4532be91d7cc5d5f642
Bryan Larsen <bryan@larsen.st>
2015-02-04 16:49:53 -0500

    let admin view files

--- app/policies/edt_file_policy.rb
+++ app/policies/edt_file_policy.rb
@@ -2,17 +2,18 @@ class EdtFilePolicy < Struct.new(:current_user, :file)
   class Scope < Struct.new(:current_user, :scope)
     def resolve
       return scope.none if current_user.blank?
+      return scope.all if current_user.admin?
       return scope.includes(:user).where(users: {agent_id: current_user.id}) if current_user.agent?
       return scope.where(user_id: current_user.id)
     end
   end
 
   def show?
-    current_user.id == file.user_id || file.user.agent_id == current_user.id
+    current_user.id == file.user_id || file.user.agent_id == current_user.id || current_user.admin?
   end
 
   def update?
-    current_user.id == file.user_id || file.user.agent_id == current_user.id
+    current_user.id == file.user_id || file.user.agent_id == current_user.id || current_user.admin?
   end
 
   def create?
