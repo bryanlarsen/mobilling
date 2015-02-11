d066dca218dc7317251fee60ee0ea16cdebe9dbb
Bryan Larsen <bryan@larsen.st>
2015-02-11 16:41:54 -0500

    be more liberal when entering time inputs to make Bethany happy

--- app/assets/javascripts/components/claim-time.js.jsx
+++ app/assets/javascripts/components/claim-time.js.jsx
@@ -1,9 +1,21 @@
 var ClaimTime = React.createClass({
+  onTextChange: function(ev) {
+    var re = /^([0-9]{0,2}?)[.:]?([0-9]{0,2})$/;
+    var match = ev.target.value.match(re);
+    if (!match) {
+      ev.target.value = "00:00";
+      this.props.onChange(ev);
+    }
+    var zf = function(s) { return ('00' + s).slice(-2); };
+    ev.target.value = zf(match[1])+':'+zf(match[2]);
+    this.props.onChange(ev);
+  },
+
   render: function() {
     return (
       <ClaimInputWrapper {...this.props}>
         <div className="input-group">
-          <ClaimInputInner type="text" {...this.props} />
+          <ClaimInputInner type="text" {...this.props} onChange={this.onTextChange}/>
           <span className="input-group-btn">
             <label className="btn btn-primary btn-block">
               <i className="fa fa-chevron-down" />
