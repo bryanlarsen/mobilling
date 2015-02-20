affe4aef172f9ad28fc4cc641870ef21aa3b3fe1
Bryan Larsen <bryan@larsen.st>
2015-02-20 12:08:55 -0500

    add dataTables dependency

--- app/assets/javascripts/react-bundle.js
+++ app/assets/javascripts/react-bundle.js
@@ -5,6 +5,7 @@
 //= require pickadate/lib/picker.time
 //= require typeahead.js/dist/typeahead.bundle
 //= require remarkable
+//= require datatables
 //= require react-bootstrap
 //= require ./react-bootstrap-globals
 //= require immutable
