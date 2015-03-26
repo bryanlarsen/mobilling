a97b90870e9ea7a08151e90ef587b7079002cb33
Bryan Larsen <bryan@larsen.st>
2015-03-26 13:02:28 -0400

    fix time/date issues: popping, requiring punctuation for dates

--- app/assets/javascripts/components/claim-date-group.js.jsx
+++ app/assets/javascripts/components/claim-date-group.js.jsx
@@ -1,13 +1,62 @@
+var normalizeDate = function(s, today) {
+  var re = /^([0-9]{0,4}?)[.,:\/-]?([0-9]{0,2}?)[.,:\/-]?([0-9]{0,2})$/;
+  var match = s.match(re);
+  if (!today) today = new Date();
+  var zf = function(s, l) { return ('00' + s).slice(-(l||2)); };
+  if (!match) {
+    return zf(today.getFullYear(),4)+'-'+zf(today.getMonth()+1)+'-'+zf(today.getDate());
+  }
+  var y = parseInt(match[1] || today.getFullYear());
+  if (y < today.getFullYear()-2000) y=y+2000;
+  if (y < 999) y=y+1900;
+  var m = parseInt(match[2] || (today.getMonth()+1));
+  var d = parseInt(match[3] || today.getDate());
+  var date = new Date(y,m-1,d); // round trip to validate
+  return zf(date.getFullYear(),4)+'-'+zf(date.getMonth()+1)+'-'+zf(date.getDate());
+};
+
 var ClaimDate = React.createClass({
+  onClick: function() {
+    event.stopPropagation();
+    var element = $(this.refs.pickerInput.getDOMNode());
+    var picker = element.pickadate({
+      format: "yyyy-mm-dd",
+      //container: ".app-body",
+      //min: attributes.min === undefined ? false : attributes.min,
+      //max: attributes.max === undefined ? false : attributes.max,
+      max: new Date(),
+      //editable: this.props.birthday,
+      selectYears: this.props.birthday ? 150 : false,
+      selectMonths: this.props.birthday
+    }).pickadate("picker");
+
+    picker.start();
+    picker.open();
+
+    picker.on('close', function() {
+      element.blur();
+      picker.stop();
+      element.attr({readOnly: true});
+      this.props.onChange({
+        target: element[0],
+      });
+    }.bind(this));
+  },
+
+  onTextChange: function(ev) {
+    ev.target.value = normalizeDate(ev.target.value);
+    this.props.onChange(ev);
+  },
+
   render: function() {
     return (
       <ClaimInputWrapper type="text" {...this.props}>
+        <input type="text" readOnly className="hide" name={this.props.name} value={this.props.value || this.props.store.get(this.props.name)} onChange={this.props.onChange} ref="pickerInput"/>
         <div className="input-group">
-          <ClaimInputInner type="text" {...this.props} />
-          <span className="input-group-btn">
+          <ClaimInputInner type="text" {...this.props} onChange={this.onTextChange}/>
+          <span onClick={this.onClick} className="input-group-btn">
             <label className="btn btn-primary btn-block">
               <i className="fa fa-chevron-down" />
-              <Pickadate className="hide" name={this.props.name} value={this.props.value || this.props.store.get(this.props.name)} readOnly birthday={this.props.birthday} onChange={this.props.onChange} />
             </label>
           </span>
         </div>
--- app/assets/javascripts/components/claim-time.js.jsx
+++ app/assets/javascripts/components/claim-time.js.jsx
@@ -1,4 +1,48 @@
 var ClaimTime = React.createClass({
+  onClick: function(event) {
+    event.stopPropagation();
+    var element = $(this.refs.pickerInput.getDOMNode());
+    var component = this;
+    var picker = element.pickatime({
+      interval: 15,
+      format: "HH:i",
+      formatLabel: function (time) {
+        var tin, tout;
+
+        if (component.props.min) {
+          tin = FeeGenerator.inMinutes(component.props.min);
+          tout = time.pick;
+        }
+
+        if (component.props.max) {
+          tin = time.pick;
+          tout = FeeGenerator.inMinutes(component.props.max);
+        }
+
+        if (tin !== undefined) {
+          var hours = (tout-tin) / 60;
+          if (hours < 0) hours = hours+24;
+          return  "HH:i <sm!all cl!ass='text-muted'>" + hours + "!h</sm!all>";
+        } else {
+          return "HH:i";
+        }
+      },
+    }).pickatime("picker");
+
+    picker.start();
+    picker.open();
+
+    picker.on('close', function() {
+      element.blur();
+      picker.stop();
+      element.attr({readOnly: true});
+      this.props.onChange({
+        target: element[0],
+      });
+    }.bind(this));
+
+  },
+
   onTextChange: function(ev) {
     var re = /^([0-9]{0,2}?)[.:]?([0-9]{0,2})$/;
     var match = ev.target.value.match(re);
@@ -14,12 +58,12 @@ var ClaimTime = React.createClass({
   render: function() {
     return (
       <ClaimInputWrapper {...this.props}>
+        <input type="text" readOnly className="hide" name={this.props.name} value={this.props.value || this.props.store.get(this.props.name)} onChange={this.props.onChange} ref="pickerInput"/>
         <div className="input-group">
           <ClaimInputInner type="text" {...this.props} onChange={this.onTextChange}/>
-          <span className="input-group-btn">
+          <span onClick={this.onClick} className="input-group-btn">
             <label className="btn btn-primary btn-block">
               <i className="fa fa-chevron-down" />
-              <Pickatime className="hide" name={this.props.name} value={this.props.value || this.props.store.get(this.props.name)} readOnly onChange={this.props.onChange} max={this.props.max} min={this.props.min}/>
             </label>
           </span>
         </div>
