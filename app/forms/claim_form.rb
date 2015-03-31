bryanlarsen/mobilling

commit 5c56ed0a5c0ece4256c7f35b38631ec953b7c96d

allow admin to change doctor

app/forms/claim_form.rb
@@ -193,6 +193,7 @@ class ClaimForm
     {
       photo_id: photo_id,
       status: status,
+      user_id: user_id,
       details: claim_attribute_details
     }
   end

commit 6deac1c12ef26772bc551f39a526cd7de56730f2

5c56ed0 broke creating new claims.  fix.

app/forms/claim_form.rb
@@ -193,7 +193,7 @@ class ClaimForm
     {
       photo_id: photo_id,
       status: status,
-      user_id: user_id,
+      user_id: user_id || user.id,
       details: claim_attribute_details
     }
   end