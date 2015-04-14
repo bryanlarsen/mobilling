9d7565c04a363f0de195ad3ee673cf7626aceefb
Bryan Larsen <bryan@larsen.st>
2015-04-14 11:39:53 -0400

    fix tests

--- test/controllers/v3/sessions_controller_test.rb
+++ test/controllers/v3/sessions_controller_test.rb
@@ -7,15 +7,14 @@ class V3::SessionsControllerTest < ActionController::TestCase
   end
 
   test "create redirects to root" do
-    create(:user, email: "alice@example.com", password: "secret")
+    create(:user, email: "alice@example.com", password: "secret", role: "doctor")
     post :create, v3_create_session: {email: "alice@example.com", password: "secret"}
     assert_redirected_to root_path
     assert @controller.current_user.present?
   end
 
   test "admin redirects to dashboard" do
-    session[:admin] = true
-    create(:user, email: "alice@example.com", password: "secret")
+    create(:user, email: "alice@example.com", password: "secret", role: "agent")
     post :create, v3_create_session: {email: "alice@example.com", password: "secret"}
     assert_redirected_to admin_dashboard_path
     assert @controller.current_user.present?
