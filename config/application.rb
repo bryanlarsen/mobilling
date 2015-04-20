b628247a0a3f8c5b9410c26303e502ba01c6af49
Bryan Larsen <bryan@larsen.st>
2015-04-20 15:50:22 -0400

    revert sprockets

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module MoBilling
   class Application < Rails::Application
-    config.angular_templates.module_name = "moBilling.templates"
     config.active_record.raise_in_transactional_callbacks = true # surpress carrierwave deprecation warnings
     config.active_support.test_order = :sorted
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
