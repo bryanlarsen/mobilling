f09734a12696f20bfb1490696534623ae64e1c86
Bryan Larsen <bryan@larsen.st>
2015-05-07 08:47:31 -0400

    only search beginning of patient number

--- app/controllers/v1/patients_controller.rb
+++ app/controllers/v1/patients_controller.rb
@@ -5,7 +5,7 @@ class V1::PatientsController < V1::BaseController
       scope = scope.where(["details->>'patient_name' ILIKE ?", "%#{params[:name]}%"])
     end
     if params[:number]
-      scope = scope.where(["details->>'patient_number' ILIKE ?", "%#{params[:number]}%"])
+      scope = scope.where(["details->>'patient_number' ILIKE ?", "#{params[:number]}%"])
     end
     patients = scope.select("DISTINCT ON (details->>'patient_number', details->>'patient_name') (details->>'patient_number') AS health_number, (details->>'patient_name') AS name, (details->>'patient_province') AS province, (details->>'patient_birthday') AS birthday, (details->>'patient_sex') AS sex").limit(10).map do |claim|
       {
