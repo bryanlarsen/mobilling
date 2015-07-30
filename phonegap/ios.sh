6d5c60f293ce31b7b85ebd0056c57b5b7af1da09
Bryan Larsen <bryan@larsen.st>
2015-07-30 10:44:33 -0400

    load fonts locally on cordova

--- phonegap/ios.sh
+++ phonegap/ios.sh
@@ -6,7 +6,7 @@
 
 cp ../public/assets/v3.js www/js
 cp ../public/assets/static.js www/js
-cp ../public/assets/v3.css www/css
+cp ../public/assets/cordova.css www/css
 cp ../public/assets/react-bundle.js www/js
 curl http://localhost:3000/v1/service_codes.js > www/js/service_codes.js
 cordova emulate ios
