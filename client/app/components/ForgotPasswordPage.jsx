8b3d0cbd55dce28f15c929da56a2e3ba67a167c5
Bryan Larsen <bryan@larsen.st>
2015-10-22 12:24:31 -0400

    missing file

---
+++ client/app/components/ForgotPasswordPage.jsx
@@ -0,0 +1,55 @@
+import { EmptyHeader, ClaimInputGroup, ClaimFormGroup } from '../components';
+import { setNotice, updateUserAttributes, userResponse } from '../actions';
+import { connect } from 'react-redux';
+import { pushState } from 'redux-router';
+
+@connect((state) => state)
+class ForgotPasswordPage extends React.Component {
+  submit(ev) {
+    ev.preventDefault();
+    var url = window.ENV.API_ROOT+'v1/request_password_reset.json';
+    $.ajax({
+      url: url,
+      data: JSON.stringify({create_password_reset: {email: this.props.userStore.email}}),
+      contentType: 'application/json',
+      dataType: 'json',
+      processData: false,
+      type: 'POST',
+      success: (data) => {
+        if (data.notice) this.props.dispatch(setNotice(data.notice));
+        this.props.dispatch(pushState('/login', '/login'));
+      },
+      error: (xhr, status, err) => {
+        if (xhr.status === 403) {
+          this.props.dispatch(pushState(null, '/login'));
+        } else if (xhr.responseJSON) {
+          this.props.dispatch(userResponse(xhr.responseJSON));
+        } else {
+          this.props.dispatch(unrecoverableError());
+        }
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="body">
+        <EmptyHeader {...this.props}/>
+
+        <div className="container with-bottom">
+          <form ref="form" className="form-horizontal" onSubmit={this.submit.bind(this)}>
+
+            <legend>Request Password Reset</legend>
+            <ClaimInputGroup store={this.props.userStore} type="email" name="email" onChange={(ev) => this.props.dispatch(updateUserAttributes({email: ev.target.value}))}/>
+
+            <ClaimFormGroup label="">
+              <button type="submit" className="btn btn-default btn-primary">Submit</button>
+            </ClaimFormGroup>
+          </form>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default ForgotPasswordPage;
