4c0b45d42c7b2b2498d832ada5876eedf20478f0
Bryan Larsen <bryan@larsen.st>
2015-10-25 22:59:31 -0400

    build updates

--- deploy/build.sh
+++ deploy/build.sh
@@ -5,5 +5,10 @@ set -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ENV=${1:-staging}
 
+cd ../gh-pages
+git pull
+./build.sh
+cd ${DIR}
+
 sudo docker build -t mobilling-${ENV} $DIR/..
 sudo docker run --env-file=../.env --env=DATABASE_URL=postgres://postgres@postgres.service.consul/mobilling_${ENV} --env=RAILS_ENV=${ENV} mobilling-${ENV} rake db:migrate
