d9ebfd93ee3ceb05986a275801780adee55acc4c
Bryan Larsen <bryan@larsen.st>
2015-10-27 10:53:24 -0400

    now unused

--- app/assets/javascripts/admin-react.js
+++
@@ -1,8 +0,0 @@
-//= require es5-shim/es5-shim
-//= require react_on_rails
-
-//= require generated/vendor-bundle
-//= require generated/admin-bundle
-
-//= require bootstrap-sprockets
-//= require turbolinks
