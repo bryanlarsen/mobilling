176a41866913264dbbb39570b4e8c04c194958e9
Bryan Larsen <bryan@larsen.st>
2015-11-05 13:53:35 -0500

    single page example

--- client/app/components/NativePatient.jsx
+++ client/app/components/NativePatient.jsx
@@ -6,7 +6,9 @@ class NativePatient extends React.Component {
     const claim = this.props.claimStore.claims[this.props.params.id];
     if (!claim) return false;
     const handler = claimChangeHandler.bind(null, this.props.dispatch, claim.id);
-    return <ClaimPatient {...this.props} store={claim} claim={claim} onChange={handler}/>;
+    return <div className="form-horizontal">
+      <ClaimPatient {...this.props} store={claim} claim={claim} onChange={handler}/>;
+    </div>;
   }
 };
 
